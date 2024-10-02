6201633eaf0f087a52cd7b281e2e4602e30fbf7e
Gustavo Miatello <miatellogmonteiro@gmail.com>
2024-10-02 16:14:28 -0400

    mudanças no script
    aula 02/10

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('O documento está pronto!');
+
+    const imagens = document.querySelectorAll('img');
+
+    imagens.forEach((imagem) => {
+        imagem.addEventListener('click', () => {
+            alert(`Você clicou na imagem: ${imagem.src}`);
+        });
+    });
+
+
+    const titulo = document.querySelector('h1');
+    titulo.addEventListener('mouseover', () => {
+        titulo.style.color = 'blue';
+    });
+
+
+    titulo.addEventListener('mouseout', () => {
+        titulo.style.color = '#333';
+    });
 });
